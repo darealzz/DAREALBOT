abd66c400f5064dde28731babed8573d63fd44bb
Pyro-YT <sikling247@gmail.com>
2020-06-13 23:24:30 +0100

    Fixed help module sensitivity issues

--- cogs/info.py
+++ cogs/info.py
@@ -14,6 +14,50 @@ class EmbedHelpCommand(commands.HelpCommand):
         self.verify_checks = False
         self.__doc__ = 'asd'
 
+    async def command_callback(self, ctx, *, command=None):
+
+        await self.prepare_help_command(ctx, command)
+        bot = ctx.bot
+
+        if command is None:
+            mapping = self.get_bot_mapping()
+            return await self.send_bot_help(mapping)
+
+        # Check if it's a cog
+        command = command.lower().title()
+        cog = bot.get_cog(command)
+        if cog is not None:
+            return await self.send_cog_help(cog)
+
+        maybe_coro = discord.utils.maybe_coroutine
+
+        # If it's not a cog then it's a command.
+        # Since we want to have detailed errors when someone
+        # passes an invalid subcommand, we need to walk through
+        # the command group chain ourselves.
+        keys = command.split(' ')
+        cmd = bot.all_commands.get(keys[0])
+        if cmd is None:
+            string = await maybe_coro(self.command_not_found, self.remove_mentions(keys[0]))
+            return await self.send_error_message(string)
+
+        for key in keys[1:]:
+            try:
+                found = cmd.all_commands.get(key)
+            except AttributeError:
+                string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                return await self.send_error_message(string)
+            else:
+                if found is None:
+                    string = await maybe_coro(self.subcommand_not_found, cmd, self.remove_mentions(key))
+                    return await self.send_error_message(string)
+                cmd = found
+
+        if isinstance(cmd, discord.ext.commands.core.Group):
+            return await self.send_group_help(cmd)
+        else:
+            return await self.send_command_help(cmd)
+
     def get_ending_note(self):
         now = datetime.datetime.now()
         if len(str(now.minute)  ) == 1:
@@ -81,7 +125,6 @@ class EmbedHelpCommand(commands.HelpCommand):
 
     async def send_cog_help(self, cog):
 
-        verify_checks=False
         cmds=""
         embed = discord.Embed(title=f'{(cog.qualified_name).upper()} MODULE', colour=self.COLOUR)
         # if cog.description:
