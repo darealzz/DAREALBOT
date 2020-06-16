c291b41982f38b581002b21de1878e0d3ca15012
Pyro-YT <tmaganti@icloud.com>
2020-06-16 17:37:11 +0100

    Create meme command\nPC switch commit

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -4,8 +4,12 @@ import time
 import sys
 import os
 import darealmodule
+import requests
+import json
+import aiohttp
+import random
 
-class Fun(commands.Cog):
+class Reddit(commands.Cog):
 
     """This Module allows give you reletive data about this bot."""
 
@@ -14,6 +18,16 @@ class Fun(commands.Cog):
         self.icon = "<:Fun:722199115680710687>"
         self.thumbnail = 'https://media.discordapp.net/attachments/714791190926458901/722198928254304415/maps-and-location.png'
 
-    
+    @commands.command(help='Generates a meme..')
+    async def meme(self, ctx):
+        """
+        Generates a meme.
+        """
+
+        meme_lst = list(self.bot.memes_cache.items())
+        meme = random.choice(meme_lst)
+        await ctx.send(meme[1])
+
+
 def setup(bot):
-    bot.add_cog(Fun(bot))
+    bot.add_cog(Reddit(bot))
